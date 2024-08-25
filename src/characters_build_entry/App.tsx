a7235eba6e91f0e1ed8a3d2383219e73cb1f12fd
Xavier Thomas <x.devr.space@gmail.com>
2024-08-24 23:30:29 -0400

    Changes the Characts_build_entry into a build time app thats only CSR

--- src/characters_build_entry/App.tsx
+++ src/characters_build_entry/App.tsx
@@ -18,7 +18,7 @@ const Index = () => (
           src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/modern-js-logo.svg"
           alt="Modern.js Logo"
         />
-        <p className="name">Modern.js</p>
+        <p className="name">characters_build_entry</p>
       </div>
       <p className="description">
         Get started by editing <code className="code">src/routes/page.tsx</code>
